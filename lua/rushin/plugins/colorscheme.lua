f762df3b05b02b39e8867c8e838c87d5e38a9018
rushin236 <rushikeshshindesandesh@gmail.com>
2024-02-27 01:12:11 +0530

    modified settings in colorscheme

--- lua/rushin/plugins/colorscheme.lua
+++ lua/rushin/plugins/colorscheme.lua
@@ -13,7 +13,6 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       require("tokyonight").setup({
-        style = "night",
         transparent = true,
         styles = {
           sidebars = "transparent",
